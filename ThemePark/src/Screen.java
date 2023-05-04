da6443d12725a4a1fea6b5901f3dbabbd9e6772c
Velossoraptor <130588823+Velossoraptor@users.noreply.github.com>
2023-05-04 19:59:38 -0400

    Added tab toggle
    Changed order of some code, and added a toggle so created tabs are not recreated.

--- ThemePark/src/Screen.java
+++ ThemePark/src/Screen.java
@@ -8,27 +8,44 @@ public class Screen implements KeyListener {
     //frame size and frame
     static private int screenX = 1420;
     static private int screenY = 1000;
-    static private boolean target = false;
+
+    //toggle for if tab is already created for game
+    static private int targetGame = 0;
+    static private int targetGame2 = 0;
+    //game frame
     private JFrame gameScreen = new JFrame();
-    //various elements
+    //game tabbed pane
     private JTabbedPane menu = new JTabbedPane();
-    //DELETE \/ LATER
-    public ImageIcon test = new ImageIcon("C:/Users/talia/OneDrive - Limestone DSB/ICS4U/ThemePark-GUI-CodeJam/ThemePark/src/sprite.png");
-    public JLabel testlabel = new JLabel(test);
-    public JPanel testPanel = new JPanel();
+    
+    //Main game components
     private ImageIcon mapImage = new ImageIcon("C:/Users/talia/OneDrive - Limestone DSB/ICS4U/ThemePark-GUI-CodeJam/ThemePark/src/game map.png");
     public Player player = new Player("Jamie");
     private JLabel map = new JLabel();
     private JPanel main = new JPanel();
 
+    //DELETE \/ LATER
+    public ImageIcon test = new ImageIcon("C:/Users/talia/OneDrive - Limestone DSB/ICS4U/ThemePark-GUI-CodeJam/ThemePark/src/sprite.png");
+    public ImageIcon test2 = new ImageIcon("C:/Users/talia/OneDrive - Limestone DSB/ICS4U/ThemePark-GUI-CodeJam/ThemePark/src/Doggo puck.JPG");
+    public JLabel testlabel = new JLabel(test);
+    public JLabel testlabel2 = new JLabel(test2);
+    public JPanel testPanel = new JPanel();
+    public JPanel testPanel2 = new JPanel();
+
+
+
     public Screen(){
         testPanel.add(testlabel);
+        testPanel2.add(testlabel2);
         main.setLayout(null);
+
         map.setIcon(mapImage);
         map.setBounds(0,0,screenX,screenY);
+
         player.setBounds(screenX/2-player.getScaledX()/2,0,player.getScaledX(),player.getScaledY());
+
         main.add(map);
         main.add(player);
+
         //move player in front
         main.setComponentZOrder(player,0);
         menu.addTab("Main Menu", main);
@@ -48,13 +65,14 @@ public class Screen implements KeyListener {
     public static int getScreenY(){
         return screenY;
     }
+
     //adds tab to tabbedpane
     public void addTab(String title, Component JPanel){
         menu.addTab(title, JPanel);
         gameScreen.add(menu);
     }
 
-    //trigger when player moved, incomplete
+    //trigger when player moved, incomplete possibly redundant
     public void checkAddTab(Boolean tabOn, String tabTitle, Component tabPanel){
         if(player.getX()<300 && player.getY()>screenY-player.getScaledY()*3){
             if(tabOn=false){
@@ -96,8 +114,16 @@ public class Screen implements KeyListener {
                 break;
         }
         
-        if(player.getX()<=player.getScaledX() && player.getY()>=screenY-player.getScaledY()*2){
+        //Check if player is in correct zone
+        if(player.getX()<=player.getScaledX() && player.getY()>=screenY-player.getScaledY()*2 && targetGame ==0){
              addTab("Target Game",testPanel);
+             menu.setSelectedIndex(1);
+             targetGame++;
+        }
+        if(player.getX()>=screenX-player.getScaledX()*2 && player.getY()>=screenY-player.getScaledY()*2 && targetGame2==0){
+            addTab("Filler name",testPanel2);
+            menu.setSelectedIndex(2);
+            targetGame2++;
         }
         
 
