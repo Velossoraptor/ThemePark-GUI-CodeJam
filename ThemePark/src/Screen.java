921f32ad925e5a2b6fdbebf1c1d02155b8e62476
Velossoraptor <130588823+Velossoraptor@users.noreply.github.com>
2023-05-05 14:13:49 -0400

    Fiddled around
    didnt change much, mostly worked with Keagan on target game through a doc, and what changes I did make I'm not using yet.

--- ThemePark/src/Screen.java
+++ ThemePark/src/Screen.java
@@ -19,7 +19,7 @@ public class Screen implements KeyListener {
     
     //Main game components
     private ImageIcon mapImage = new ImageIcon("C:/Users/talia/OneDrive - Limestone DSB/ICS4U/ThemePark-GUI-CodeJam/ThemePark/src/game map.png");
-    private Player player = new Player("Jamie");
+    public Player player = new Player("Jamie");
     private JLabel map = new JLabel();
     private JPanel main = new JPanel();
 
@@ -34,6 +34,7 @@ public class Screen implements KeyListener {
 
 
     public Screen(){
+        //gameScreen.add(new subMenu("Menu", true));
         testPanel.add(testlabel);
         testPanel2.add(testlabel2);
         main.setLayout(null);
--- ThemePark/src/subMenu.java
+++ ThemePark/src/subMenu.java
@@ -1,13 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class subMenu extends JInternalFrame {
-    private JTextField userNameIn = new JTextField("Username");
+public class subMenu extends JInternalFrame implements ActionListener{
+    //private JTextField userNameIn = new JTextField("Username",20);
     private String userName;
+    private JButton girl = new JButton();
+    private JButton boy = new JButton();
 
-    public subMenu(){
+    public subMenu(String title, boolean closeable){
         this.setSize(500,500);
-        this.add(userNameIn);
-        userName=userNameIn.getText();
+        this.setClosable(closeable);
+        this.setTitle(title);
+        boy.addActionListener(e -> handleBoy());
+
+        this.add(girl);
+        this.add(boy);
+        //this.add(userNameIn);
+        //userName=userNameIn.getText();
+        this.setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    public void handleBoy(){
+        Player.setIcon(playerBoy);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
